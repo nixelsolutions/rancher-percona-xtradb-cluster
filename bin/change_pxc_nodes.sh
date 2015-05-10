837e84f816e31b57165a1f09ce72b8b2f5bdb847
root <root@ip-172-31-33-4.us-west-2.compute.internal>
2015-05-10 20:52:07 +0000

    first commit

--- bin/change_pxc_nodes.sh
+++ bin/change_pxc_nodes.sh
@@ -5,7 +5,7 @@ set -e
 [ "$DEBUG" == "1" ] && set -x && set +e
 
 PXC_NODES=$1
-if [ "${PXC_NODES}" == "**ChangeMe**" ] || [ -z ${PXC_NODES} ]; then
+if [ "${PXC_NODES}" == "**ChangeMe**" ] || [ -z "${PXC_NODES}" ]; then
    echo "ERROR: You did not specify nodes to join PXC cluster, please enter PXC nodes as an argument."
    echo "You may specify more than one node separated by a comma, for example: $0 X.X.X.X,Y.Y.Y.Y"
    echo "Exiting..."
