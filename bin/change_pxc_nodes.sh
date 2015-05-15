c5d1805a665b6802496140c8ad604311434c0607
Manel Martinez Gonzalez <manel@MacBook-de-Manel.local>
2015-05-15 20:47:20 +0200

    first commit

--- bin/change_pxc_nodes.sh
+++ bin/change_pxc_nodes.sh
@@ -17,5 +17,5 @@ fi
 
 perl -p -i -e "s/wsrep_cluster_address\s*=\s*gcomm:\/\/.*/wsrep_cluster_address = gcomm:\/\/$PXC_NODES/g" ${PXC_CONF}
 if [ -e ${PXC_CONF_FLAG} ]; then
-   perl -p -i -e "s/PXC_NODES=.*/PXC_NODES=${PXC_NODES}/g" ${PXC_CONF_FLAG}
+   perl -p -i -e "s/PXC_NODES=.*/PXC_NODES=\"${PXC_NODES}\"/g" ${PXC_CONF_FLAG}
 fi
