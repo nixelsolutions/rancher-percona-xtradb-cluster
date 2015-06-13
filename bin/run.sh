dea5c88b054f4f09c93099786b2dae0141c28bc8
Manel Martinez Gonzalez <manel@MacBook-de-Manel.local>
2015-06-13 23:39:38 +0200

    Improved bootstraping for scaled up nodes using Rancher Services

--- bin/run.sh
+++ bin/run.sh
@@ -59,7 +59,7 @@ if [ ! -e ${PXC_CONF_FLAG} ]; then
          continue
       fi
       # Check if node is already initializated - that means the cluster has already been bootstraped 
-      if wget -O- -T 20 http://${node}:${MYSQLCHK_PORT}" >/dev/null 2>&1; then
+      if wget -O- -T 20 http://${node}:${MYSQLCHK_PORT} >/dev/null 2>&1; then
          BOOTSTRAPED=true
          break
       fi
